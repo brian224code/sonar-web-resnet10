fe9e591325bf9fa9386f79315138413d1be84777
Angel Patricio <aopatric@mit.edu>
2025-01-19 07:41:24 -0500

    add weight export and show weights

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ let dataSet = null
 
 const loadDataBtn = document.getElementById('loadData')
 const startTrainingBtn = document.getElementById('startTraining')
+const dumpWeightsBtn = document.getElementById('dumpWeights')
 
 // initial page loading
 document.addEventListener('DOMContentLoaded', () => {
@@ -60,8 +61,46 @@ startTrainingBtn.addEventListener('click', async () => {
 		addLog(`Error during training:`, error)
 	}
 
-	startTrainingBtn.disabled = false
-	loadDataBtn.disabled = false
-
+	dumpWeightsBtn.disabled = false
+	
 	addLog("Done.")
+})
+
+dumpWeightsBtn.addEventListener('click', async () => {
+	addLog("Dumping weights...")
+	const weightsJSON = {
+		weights: [],
+		shapes: [],
+		names: []
+	};
+	const layers = model.model.layers
+	addLog(`Model layers: ${layers.length}`)
+
+	for (let i = 0; i < layers.length; i++) {
+		const layer = layers[i]
+		const layerWeights = layer.getWeights()
+
+		for (let j = 0; j < layerWeights.length; j++) {
+			const weightTensor = layerWeights[j]
+			const weightValues = await weightTensor.data(0)
+
+			weightsJSON.weights.push(Array.from(weightValues))
+			weightsJSON.shapes.push(weightTensor.shape)
+			weightsJSON.names.push(`${layer.name}_weight_${j}`)
+		}
+	}
+
+	addLog(`Done processing. Downloading...`)
+
+	const weightString = JSON.stringify(weightsJSON, null, 2)
+
+	const blob = new Blob([weightString], { type: 'application/json' })
+	const url = URL.createObjectURL(blob)
+	const a = document.createElement('a')
+	a.href = url
+	a.downoad = 'model_weights.json'
+	document.body.appendChild(a)
+	a.click()
+	document.body.removeChild(a)
+	URL.revokeObjectURL(url)
 })
